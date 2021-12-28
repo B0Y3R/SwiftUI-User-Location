2e158ac07c716e33c423242bf5da18249f8b443b
James Boyer <jamesboyer92@user.noreply.github.com>
2021-12-27 19:32:03 -0500

    Initial Commit

---
+++ SwiftUI-User-Location/SwiftUI_User_LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_User_LocationApp.swift
+//  SwiftUI-User-Location
+//
+//  Created by James Boyer on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_User_LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
